c7f59d66200975030cd6196291e470aa8ab15614
yael <yael.atletl@gmail.com>
2020-06-04 17:26:54 -0500

    expanded definitions,

--- Definitions.gd
+++ Definitions.gd
@@ -55,8 +55,32 @@ var CLASS_CHARACTER =  {
 		"target_location" : TYPE_VECTOR3}
 	}
 #Do not use spaces or symbols in function defintions. 
+#_s_ prefix indicates the label would be replaced with a special component
+#This prefix only works in the input label.
 var _functions = {
-	"Tri_v_Decision" : {
+	"property_check" : {
+		"_category" : "inhibitors",
+		"_code" : "FuncRef?", #This hasn't been implemented yet
+		"_input_ports" : [
+			{"_label_title":"Object input", "_type" : TYPE_OBJECT},
+			{"_label_title":"_s_prop_dropdown_CHARACTER", "_type" : TYPE_NIL}
+		],
+		"_output_ports" : [
+			{"_label_title" : "Property content", "_type" : Nodes.TYPE_ANY},
+		]
+	},
+	"match" : {
+		"_category" : "inhibitors",
+		"_code" : "FuncRef?", #This hasn't been implemented yet
+		"_input_ports" : [
+			{"_label_title":"String input", "_type" : TYPE_STRING},
+			{"_label_title":"_s_text_edit", "_type" : TYPE_NIL}
+		],
+		"_output_ports" : [
+			{"_label_title" : "Does Match", "_type" : TYPE_BOOL },
+		]
+	},
+	"tri_v_decision" : {
 		"_category" : "inhibitors",
 		"_code" : "FuncRef?", #This hasn't been implemented yet
 		"_input_ports" : [
@@ -83,12 +107,40 @@ var _functions = {
 			{"_label_title":"title","_type":TYPE_NIL},
 			{"_label_title":"title2","_type":TYPE_NIL}
 			]
+	}, 
+	"set_objective" : {
+		"_category" : "actions",
+		"_code" : "<actual code>",
+		"_input_ports" : [
+			{"_label_title":"Objective position","_type":TYPE_VECTOR3},
+			],
+		"_output_ports" : [
+			{"_label_title":"","_type":TYPE_NIL},
+			]
+	},
+	"force_next_state" : {
+		"_category" : "actions",
+		"_code" : "<actual code>",
+		"_input_ports" : [
+			{"_label_title":"Trigger","_type":Nodes.TYPE_ANY},
+			],
+		"_output_ports" : [
+			{"_label_title":"","_type":TYPE_NIL},
+			]
 	} 
 }
 
 var _stimulus = {
-	"NewHear" :{
+	"hear" :{
 		"_output_name": "Emmiter Data",
-		"_output_type": "CLASS_MISC"
+		"_output_type": TYPE_OBJECT,
+	},
+	"user_input" :{
+		"_output_name": "Action Name",
+		"_output_type": TYPE_STRING,
+	},
+	"see" :{
+		"_output_name": "Object seen",
+		"_output_type": TYPE_OBJECT,
 	}
 }
